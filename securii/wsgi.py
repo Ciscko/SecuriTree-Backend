a827d0428532c46def42e307ccad94d111ccdced
Francis K <kiragufrancis97@gmail.com>
2022-01-08 09:07:28 +0300

    removed one req

--- securii/wsgi.py
+++ securii/wsgi.py
@@ -12,10 +12,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'securii.settings')
-from whitenoise import WhiteNoise
+#csfrom whitenoise import WhiteNoise
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='static/')
+#application = WhiteNoise(application, root='static/')
 
 from dotenv import load_dotenv
 # loads the configs from .env
