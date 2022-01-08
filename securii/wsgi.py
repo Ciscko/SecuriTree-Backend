b9e0714651023ee9ab3fc8094ff35cebf993ab3d
Francis K <kiragufrancis97@gmail.com>
2022-01-08 08:51:15 +0300

    added white

--- securii/wsgi.py
+++ securii/wsgi.py
@@ -12,8 +12,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'securii.settings')
+from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
 
 from dotenv import load_dotenv
 # loads the configs from .env
