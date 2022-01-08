7ea053e5dd51782b8693e79523055cdff630f8f4
Francis K <kiragufrancis97@gmail.com>
2022-01-08 08:11:50 +0300

    settings....

--- securii/settings.py
+++ securii/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
